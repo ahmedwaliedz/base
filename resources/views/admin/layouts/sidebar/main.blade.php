77073d7f225470aa072d3e5d64f441f9c290ac88
gethub23 <aa926626@gmail.com>
2025-05-18 15:30:42 +0300

    feat refactor sidebar , add active link

--- resources/views/admin/layouts/sidebar/main.blade.php
+++ resources/views/admin/layouts/sidebar/main.blade.php
@@ -1,7 +1,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-    <div class="app-brand demo">
-        <a href="{{route('admin.home')}}" class="app-brand-link w-100">
-            <img class="w-100 " style="object-fit: contain" height="50px" src="{{cache()->get('settings')['logo']}}" alt="{{cache()->get('settings')['name'][adminLang()]}}">
+    <div class="app-brand demo mt-3">
+        <a href="{{route('admin.home')}}" class="app-brand-link w-100 h-100 d-flex align-items-center justify-content-center">
+            <img class="mw-100 h-100" style="object-fit: contain" src="{{cache()->get('settings')['logo']}}" alt="{{cache()->get('settings')['name'][adminLang()]}}">
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
