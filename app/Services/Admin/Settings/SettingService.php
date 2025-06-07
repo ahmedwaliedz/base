e8085508d7928ed9a89e6c51896be33f338a980e
gethub23 <aa926626@gmail.com>
2025-06-07 05:21:31 +0300

    feat roles-permissions (handle show blade )

--- app/Services/Admin/Settings/SettingService.php
+++ app/Services/Admin/Settings/SettingService.php
@@ -36,7 +36,15 @@ class SettingService
 
     public static function setTypeAttribute($value)
     {
-         return match (gettype($value)) {
+        // Check if the value is an uploaded file and it's an image
+        if ($value instanceof \Illuminate\Http\UploadedFile && $value->isValid()) {
+            if (str_starts_with($value->getMimeType(), 'image/')) {
+                return SettingTypeEnum::IMAGE;
+            }
+            return SettingTypeEnum::FILE;
+        }
+
+        return match (gettype($value)) {
             SettingTypeEnum::JSON->value       => SettingTypeEnum::JSON,
             SettingTypeEnum::ARRAY->value      => SettingTypeEnum::JSON,
             SettingTypeEnum::INTEGER->value    => SettingTypeEnum::INTEGER,
