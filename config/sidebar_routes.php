33f1872c046a0188072bb19dc477275d2ca01386
AAIT <aa926626@gmail.com>
2025-07-09 13:28:55 +0300

    feat profile ( show , update main data , update password)

--- config/sidebar_routes.php
+++ config/sidebar_routes.php
@@ -6,16 +6,17 @@ return [
             'icon' => '<i class="ti ti-home me-2"></i>',
             'has_child' => false,
         ],
-        'notifications' => [
-            'icon'      => '<i class="ti ti-mail me-2"></i>',
-            'has_child' => true,
-            'childes'   => [],
-        ],
         'settings' => [
             'icon'      => '<i class="ti ti-settings me-2"></i>',
             'has_child' => true,
             'childes'   => [],
         ],
+        'notifications' => [
+            'icon'      => '<i class="ti ti-mail me-2"></i>',
+            'has_child' => true,
+            'childes'   => [],
+        ],
+
         'users' => [
             'has_child'    => true,
             'icon'         => '<i class="ti ti-users me-2"></i>',
